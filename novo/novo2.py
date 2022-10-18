d6195651172cb59863f6a4b2325d549186be0e8f
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-10-17 22:49:34 -0300

    first commit

---
+++ novo/novo2.py
@@ -0,0 +1,15 @@
+import sys
+from design import *
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Calc(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    calc = Calc()
+    calc.show()
+    qt.exec_()
